6570b471dfc835231ecf5c24d44a2569f2dab6c7
Ziggie <mansur2k04@mail.ru>
2021-08-09 02:12:35 +0300

    Занёс 15~ таблеток в базу, реализовал работоспособность addDrug контроллера(Изображения можно как URL, так и загружать самому)

--- controllers/drugs.controller.js
+++ controllers/drugs.controller.js
@@ -1,9 +1,10 @@
 const Drug = require("../models/Drug.model");
+const { extname } = require("path");
 
 module.exports.drugsController = {
   getDrugs: async (req, res) => {
     try {
-      const list = await Drug.find({}, "-__v").populate("category", "-__v -_id");
+      const list = await Drug.find().populate("category", "-_id");
       res.render(main, { list });
     } catch {
       res.json({  error: "Не удалось получить список лекарств"})
@@ -11,7 +12,7 @@ module.exports.drugsController = {
   },
   getDrugsByCategory: async (req, res) => {
     try {
-      const list = await Drug.find({  category: req.params.categoryId }, "-__v").populate("category", "-__v -_id");
+      const list = await Drug.find({ category: req.params.categoryId }).populate("category");
       res.json(list);
     } catch {
       res.json({  error: "Не удалось получить список лекарств"})
@@ -19,7 +20,7 @@ module.exports.drugsController = {
   },
   getDrugById: async (req, res) => {
     try {
-      const information = await Drug.findById(req.params.drugId, "-__v").populate("category", "-_id -__v");
+      const information = await Drug.findById(req.params.drugId).populate("category");
       res.json(information);
     } catch {
       res.json({  error: "Не удалось получить список лекарств"})
@@ -28,12 +29,20 @@ module.exports.drugsController = {
 
   addDrug: async (req, res) => {
     try {
-      await Drug.create({
-        name: req.body.name,
-        category: req.body.category,
-        price: req.body.price,
-        needPrescription: req.body.needPrescription
-      })
+      let image = req.body.image ? req.body.image : req.files.image; //Загрузка изображения по URL, либо из локального диска
+      if (typeof image === 'object') {
+
+        const imageFile = req.files.image;
+        const ext = extname(imageFile.name);
+        const newName = "/images/" + Math.random() + ext
+
+        await imageFile.mv(`./public${newName}`);
+        image = newName;
+        //Может выскочить ошибка, если файл 'images' не создан
+      }
+      const { name, category, price, needPrescription } = req.body; //Извлекает конкретные ключи из req.body
+      await Drug.create({ name, category, price, needPrescription, image}); //Ошибки могут возникать, если название ключа в модели не совпадает с названием передаваемой вами переменной
+
       res.send("Лекарство успешно добавлено в БД");
     } catch {
       res.json({  error: "Не удалось добавить лекарство в БД"})
